0ae1a2abc4d43ec8fb34630c7f60edc5f76e94a9
Chlodwig-tech <szczepan.chojnowski@tutanota.com>
2024-09-03 22:57:49 +0200

    use gpu uuid

--- examples/runner.py
+++ examples/runner.py
@@ -8,13 +8,14 @@ problem = sys.argv[1]
 sizes      = [2 ** (10 + i) for i in range(6)]
 mutations  = [1, 5, 10, 20]
 crossovers = [10, 25, 50, 75]
+id = 'GPU UUID'
 
 for size in sizes:
     for mutation in mutations:
         for crossover in crossovers:
             filename = f"results/{problem}/output-{size}-{mutation}-{crossover}.txt"
             if not (os.path.exists(filename) and os.path.getsize(filename) > 0):
-                command = [f'examples/{problem}', f'{size}', f'{mutation}.0', f'{crossover}.0']
+                command = [f'examples/{problem}', f'{size}', f'{mutation}.0', f'{crossover}.0', id]
                 print(command, datetime.datetime.now().strftime("%H:%M:%S"))
                 subprocess.run(command)
             else:
